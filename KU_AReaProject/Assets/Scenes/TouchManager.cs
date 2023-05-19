4da2ca44cbab47032accef580d5e6a8b716b6b30
pythagorachu <z0523@naver.com>
2023-05-19 15:57:46 +0900

    v1.0b
    카메라 평면인식과 터치이벤트 객체생성 +오리 오브젝트로 변경

--- KU_AReaProject/Assets/Scenes/TouchManager.cs
+++ KU_AReaProject/Assets/Scenes/TouchManager.cs
@@ -7,7 +7,8 @@ using UnityEngine.XR.ARSubsystems;
 
 public class TouchManager : MonoBehaviour
 {
-
+    // 오리 오브젝트 선언
+    public GameObject rubberduck;
     private GameObject placeObject;
     private ARRaycastManager raycastMng;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
@@ -15,13 +16,19 @@ public class TouchManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         // 인식된 바닥평면에 터치이벤트로 생성할 큐브 할당
         placeObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
         // 큐브 크기 설정
         placeObject.transform.localScale = Vector3.one * 0.05f;
+        */
+        
+        // 생성할 오브젝트에 오리 오브젝트 할당
+        placeObject = rubberduck;
+        
         // AR Raycast Manager 추출
         raycastMng = GetComponent<ARRaycastManager>();
-        
+           
     }
 
     // Update is called once per frame
@@ -33,7 +40,7 @@ public class TouchManager : MonoBehaviour
             // 평면 인식된 곳만 Ray로 검출하기
             if (raycastMng.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
             {
-                Instantiate(placeObject, hits[0].pose.position, hits[0].pose.rotation);
+                Instantiate(placeObject, hits[0].pose.position, Quaternion.Euler(-90f, 180f, 0f));
             }
         }
     }
